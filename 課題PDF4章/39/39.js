09b1c17f492fccab493ef0805735bf4e04c0706a
KoyamaTakemi <koyamatakemi.abc@gmail.com>
2022-05-04 00:21:58 +0900

    20220503学習内容3（39　メソッド・引数・コールバック関数　途中）

---
+++ 課題PDF4章/39/39.js
@@ -0,0 +1,65 @@
+// const teacher = {
+// //    // プロパティー 
+//    name: "太郎", 
+// //    // プロパティー 
+//    subject: "国語", 
+// //    // メソッド 
+//    profile: function() {
+//       console.log("担当教科は国語です"); 
+//     } 
+//   };
+//   console.log(teacher.subject);
+
+// function sumNum01(x) {
+//   const result01 = x + 1
+//   console.log(result01);
+//  };
+//  sumNum01(1);
+ 
+//  function sumNum02(x, y) {
+//   const result02 = x + y
+//   console.log(result02);
+//  };
+//  sumNum02(5, 10);
+
+//  function showText(x) {
+//   console.log(x);
+//  };
+//  showText(`引数`);
+ 
+// // コールバック関数
+//  function tomato(price, func) {
+//   const name = "トマト";
+//   func(name, price);
+//  }
+//  const price = function price(name, price) {
+//   console.log(name + " の値段は" + price + "円です。");
+//  }
+//  tomato(100, price);
+ 
+
+// ★★★★　以下課題　★★★★★
+// 1.プロパティとメソッドの違い
+// オブジェクトのkeyとvalueのvalueが関数になっているのがメソッド。
+// プロパティはオブジェクトの「状態」を示し、メソッドはオブジェクトの動作を示す。
+
+
+// 2.下記関数を実行し、結果をコンソールに表示
+function fruit(name, price) {
+  const result = name + "の値段は" + price + "円です。"
+  return result
+ };
+console.log("課題2　" + fruit("オレンジ", 50)); 
+
+// 3.下記関数を経由して2つの関数を実行
+function addTax(price, func) {
+  // priceは値段
+  // funcは実⾏する関数名
+  // 税込み価格を計算して四捨五⼊して整数にしている(スコープ内でしか参照できません。)
+  const taxPrice = Math.round(price * 1.10);
+ };
+ // トマトの税込み価格をコンソールに表⽰させる処理
+
+
+ // ⽟ねぎの税込み価格をコンソールに表⽰させる処理
+ 
